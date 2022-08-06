88e86de7e53d7ffe1f9add11fe2197771753f389
AM7999 <AMartin12122019@Outlook.com>
2022-08-05 17:28:19 -0700

    Update main.lua
    hopefully it actually does stuff now

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,29 @@
 #local video
 
+function crash()
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+    love.graphics.newVideo("video/iamfmb.ogv")
+end
+
 video = love.graphics.newVideo("video/iamfmb.ogv")
 
 love.graphics.draw(video, 0, 0)
 
 video:play()
+
+if not video:isPlaying() then
+    crash()
+end
